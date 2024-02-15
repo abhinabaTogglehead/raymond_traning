233ac251e679a58280d60c4b05e5a09d3aec0ee6
[abhinabaTogglehead,abhinaba900 <[abhinaba@togglehead.in,abhinabajana900@gmail.com>
2024-02-15 20:11:53 +0530

    spacing added

--- src/Components/Image/index.js
+++ src/Components/Image/index.js
@@ -1,5 +1,5 @@
-import image1 from "./Screenshot 2024-02-15 182845 - Copy.png";
-import image2 from "./Screenshot 2024-02-15 182855 - Copy.png";
-import image3 from "./Screenshot 2024-02-15 182919.png";
+import image1 from "./svgexport-2.svg";
+import image2 from "./svgexport-3.svg";
+import image3 from "./svgexport-4.svg";
 
 export { image1, image2, image3 };
